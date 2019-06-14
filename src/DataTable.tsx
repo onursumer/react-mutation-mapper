ecce66326c6807a73c44b29403b4647d0ccd07cc
Onur Sumer <s.onur.sumer@gmail.com>
2019-06-14 16:55:17 -0400

    added customizable highlight colors

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -17,6 +17,9 @@ export interface IDataTableProps<T> {
     initialSortColumn?: string;
     initialSortDirection?: 'asc'|'desc';
     initialItemsPerPage?: number;
+
+    highlightColorLight?: string;
+    highlightColorDark?: string;
 }
 
 @observer
@@ -25,12 +28,19 @@ export default class DataTable<T> extends React.Component<IDataTableProps<T>, {}
     public static defaultProps = {
         data: [],
         initialSortDirection: "desc",
-        initialItemsPerPage: 10
+        initialItemsPerPage: 10,
+        highlightColorLight: "#B0BED9",
+        highlightColorDark: "#9FAFD1"
     };
 
     @computed
     get columns(): Column[] {
         return this.props.columns || [];
+    }
+
+    @computed
+    get needToCustomizeRowStyle() {
+        return this.props.dataStore && this.props.dataStore.highlightFilters;
     }
 
     public render()
@@ -55,10 +65,7 @@ export default class DataTable<T> extends React.Component<IDataTableProps<T>, {}
                 <ReactTable
                     data={data}
                     columns={this.columns}
-                    getTrProps={
-                        this.props.dataStore && this.props.dataStore.highlightFilters.length >= 0 ?
-                            this.getTrProps : undefined
-                    }
+                    getTrProps={this.needToCustomizeRowStyle ? this.getTrProps : undefined}
                     defaultSorted={defaultSorted}
                     defaultPageSize={data.length > initialItemsPerPage! ? initialItemsPerPage : data.length}
                     showPagination={showPagination}
@@ -73,16 +80,27 @@ export default class DataTable<T> extends React.Component<IDataTableProps<T>, {}
     }
 
     @autobind
-    protected getTrProps(state: any, row: RowInfo) {
+    protected getTrProps(state: any, row: RowInfo)
+    {
         return {
             style: {
-                background: state && this.isRowHighlighted(row.original) ? "red" : undefined
+                background: state && this.getRowBackground(row)
             }
         };
     }
 
-    @autobind
     protected isRowHighlighted(datum: T) {
         return this.props.dataStore && this.props.dataStore.dataHighlightFilter(datum);
+    }
+
+    protected getRowBackground(row: RowInfo)
+    {
+        let background: string | undefined;
+
+        if (this.isRowHighlighted(row.original)) {
+            background = row.index % 2 === 1 ? this.props.highlightColorDark : this.props.highlightColorLight;
+        }
+
+        return background;
     }
 }
